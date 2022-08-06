ec36ee985b2dc1cefd31a0b554ad1941c9eaa157
YJ You <86042155+DEVIt-Jay-You@users.noreply.github.com>
2022-08-06 17:07:54 +1200

    Add input form

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+import InputField from './components/InputField'
 
-function App() {
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>('')
+
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <span className='heading'>Tasker</span>
+      <InputField todo={todo} setTodo={setTodo}/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
